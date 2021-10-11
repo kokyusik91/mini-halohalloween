8f2f1bd3214840a650f3136024cc351618e68ceb
dam-lee <studymida88@gmail.com>
2021-10-12 01:24:23 +0900

    feat. 로그인 회원가입 컴포넌트 UI 작업

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route } from "react-router-dom";
-import Footer from "./components/Footer";
 import Hedaer from "./components/Header";
+import Footer from "./components/Footer";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
       <Hedaer />
       <Route path="/" exact component={Main} />
       <Route path="/signup" exact component={Signup} />
+      <Route path="/login" exact component={Login} />
       <Footer />
     </BrowserRouter>
   );
--- src/elements/index.js
+++ src/elements/index.js
@@ -1,4 +1,8 @@
 import Container from "./Container";
 import Grid from "./Grid";
+import Input from "./Input";
+import Textarea from "./Textarea";
+import Upload from "./Upload";
+import Label from "./Label";
 
-export { Container, Grid };
+export { Container, Grid, Input, Textarea, Upload, Label };
