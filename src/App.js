414670426990b4d5d54784b42c354442818fea72
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-10-12 13:47:50 +0900

    README.md

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route } from 'react-router-dom';
 import Hedaer from './components/Header';
 import Footer from './components/Footer';
@@ -6,15 +5,6 @@ import Main from './pages/Main';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Postlist from './pages/Postlist';
-=======
-import { BrowserRouter, Route } from "react-router-dom";
-import Hedaer from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./pages/Main";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-
->>>>>>> 10f5c273372e86cfdc73596eac470ffb2789aedf
 function App() {
   return (
     <BrowserRouter>
