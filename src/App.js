5ddbb6fccdc7af97de124b2567548dd823a8e843
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-10-12 13:38:48 +0900

    feat. Postlist,Post.js 추가 및 App.js에 router 연결, Modal.js 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter, Route } from "react-router-dom";
-import Hedaer from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./pages/Main";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Hedaer from './components/Header';
+import Footer from './components/Footer';
+import Main from './pages/Main';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Postlist from './pages/Postlist';
 function App() {
   return (
     <BrowserRouter>
       <Hedaer />
-      <Route path="/" exact component={Main} />
-      <Route path="/signup" exact component={Signup} />
-      <Route path="/login" exact component={Login} />
+      <Route path='/' exact component={Main} />
+      <Route path='/signup' exact component={Signup} />
+      <Route path='/login' exact component={Login} />
+      <Route path='/postlist' exact component={Postlist} />
       <Footer />
     </BrowserRouter>
   );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Grid, Label, Input, Textarea, Upload } from '../elements/index';
 
 const Modal = (props) => {
-  const {} = props;
-  return <div></div>;
+  return (
+    <>
+      <ModalParent>
+        <Grid
+          width='30vw'
+          height='60vh'
+          margin='20vh auto'
+          padding='40px 50px'
+          bg='#f1f3f5'
+        >
+          글을 써주세요!
+          <Grid margin='20px 0 0 0'>
+            <Input type='file' placeholder='업로드' />
+          </Grid>
+          <Grid margin='20px 0 0 0'>
+            <Label margin=''>제목</Label>
+            <Input type='text' placeholder='제목을 입력해주세요!' />
+          </Grid>
+          <Grid margin='20px 0 0 0'>
+            <Label margin=''>게시글</Label>
+            <Textarea placeholder='게시글을 작성해주세요!' />
+          </Grid>
+          <Grid margin='20px 0 0 0'>
+            <button>글올리기</button>
+            <button>취소</button>
+          </Grid>
+        </Grid>
+      </ModalParent>
+    </>
+  );
 };
+
+const ModalParent = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 999;
+`;
 
 export default Modal;
