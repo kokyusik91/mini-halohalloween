e95dd286bed975890363216b9c3d388d3f9fbbb0
dam-lee <studymida88@gmail.com>
2021-10-14 13:07:14 +0900

    edit. 코드 병합

--- src/pages/CommentList.js
+++ src/pages/CommentList.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Grid, Button, Input } from "../elements";
 import Comment from "../components/Comment";
 import { useDispatch, useSelector } from "react-redux";
-import { actionCreators as commentActions } from "../redux/modules/comment";
+// import { actionCreators as commentActions } from "../redux/modules/comment";
 
 //1. login 했을때만 commentcard가 보임 => useState 이용 is_login  삼항연산자넣기
 //2. input값을 useEffect를 이용해서, inputdata가 변할때만  실행시켜줌
@@ -17,7 +17,7 @@ const CommentList = () => {
   React.useEffect(() => {
     if (comment_list.length === 0) {
       //디스패치로 액션함수 불러오기
-      dispatch(commentActions.getPostFB());
+      // dispatch(commentActions.getPostFB());
     }
   }, []);
 
@@ -40,7 +40,7 @@ const CommentList = () => {
           }}
           value={input}
         />
-        <Button width='auto' _onClick={onClick}>
+        <Button width="auto" _onClick={onClick}>
           등록
         </Button>
       </Grid>
