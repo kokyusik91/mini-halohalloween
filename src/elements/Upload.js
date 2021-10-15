fdc3cd3e829a690c2efbd481bdcff952142b07ed
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2021-10-15 16:40:59 +0900

    edit. 이미지 업로드 수정

--- src/elements/Upload.js
+++ src/elements/Upload.js
@@ -1,7 +1,11 @@
 /* eslint-disable */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
+import Button from '../elements/Button';
+import { storage } from '../shared/firebase';
+import { actionCreators as imageActions } from '../redux/modules/image';
+import { useDispatch } from 'react-redux';
 
 const Upload = (props) => {
   const { type, color, backgroundColor } = props;
@@ -11,12 +15,34 @@ const Upload = (props) => {
     backgroundColor: backgroundColor,
   };
 
+  // Input태그에 접근하기 위해 useRef()사용.
+  const fileInput = useRef();
+  const dispatch = useDispatch();
+
+  // onChange 함수에서 선택한 파일의 정보를 알 수 있다.
+  const selectFile = (e) => {
+    console.log('e', e);
+    console.log('e.target', e.target);
+    console.log('e.target.files', e.target.files[0]);
+    // Ref로 접근가능한지 확인
+    console.log('useRef', fileInput.current.files[0]);
+    let image = fileInput.current.files[0];
+    console.log('미들웨어로 넘겨줄 파일객체', image);
+    dispatch(imageActions.uploadImageFB(image));
+  };
+
   return (
     <React.Fragment>
       <LabelFile htmlFor='ex_file' {...styles}>
         업로드
       </LabelFile>
-      <UploadForm id='ex_file' type={type} {...styles} />
+      <UploadForm
+        id='ex_file'
+        type={type}
+        {...styles}
+        onChange={selectFile}
+        ref={fileInput}
+      />
     </React.Fragment>
   );
 };
