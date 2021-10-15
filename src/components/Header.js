8c0400f9a119f176c3dc14c6e0c068569f6d2107
dam-lee <studymida88@gmail.com>
2021-10-15 16:51:28 +0900

    edit. 머지받기 위한 커밋

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,8 @@ const Hedaer = (props) => {
   const dispatch = useDispatch();
   const is_user = useSelector((state) => state.user.user);
   const is_login = useSelector((state) => state.user.is_login);
-  console.log("header is_user = ", is_user);
+  console.log("is_user ?? == ", is_user);
+  console.log("is_login == ", is_login);
   const logout = () => {
     dispatch(userActions.logOutFB());
   };
